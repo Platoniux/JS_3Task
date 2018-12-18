2e97ec65da41aa01b9eaa6eec42692df06c9030c
Platoniux <chizzza@gmail.com>
2018-12-18 20:56:36 +0200

    improve shakeArray

--- js/task4.js
+++ js/task4.js
@@ -2,18 +2,10 @@
 
 var randomArray = [5, 6, 9, "hello world", true];
 
-
-shakeArray(randomArray);
-
-
-function shakeArray(arr) {
-  var index = 0;
-  for (var i = 0; i < arr.length; i++) {
-    index = Math.floor(Math.random() * arr.length);
-    var itemAtIndex = arr[index];
-    arr[index] = arr[i];
-    arr[i] = itemAtIndex; 
-  }
-  console.log(arr);
+function shakeArray(a, b) {
+  return Math.random() - 0.5;
 }
+
+randomArray.sort(shakeArray);
+console.log(randomArray);
 
