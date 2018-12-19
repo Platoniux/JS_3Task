aad1f816869420cf2801c5bf6dcf08d4286bb332
Platoniux <chizzza@gmail.com>
2018-12-19 07:56:40 +0200

    corrected style

--- js/task4.js
+++ js/task4.js
@@ -2,10 +2,11 @@
 
 var randomArray = [5, 6, 9, "hello world", true];
 
+randomArray.sort(shakeArray);
+console.log(randomArray);
+
+
 function shakeArray(a, b) {
   return Math.random() - 0.5;
 }
-
-randomArray.sort(shakeArray);
-console.log(randomArray);
 
